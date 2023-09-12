d4330b89b0f0e762d045984f5359fc007cc03d39
Adonijah01 <adonijahsingoei@gmail.com>
2023-09-12 08:49:20 -0700

    javascript objects scopes closures

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+class Rectangle {
+constructor (w, h) {
+if ((w > 0) && (h > 0)) {
+
+      this.width = w;
+
+      this.height = h;
+
+    }
+
+  }
+
+}
+module.exports = Rectangle;
+// adonijah kiplimo
---
+++ 0x13-javascript_objects_scopes_closures/README.md
@@ -0,0 +1,2 @@
+My 0x13. JavaScript - Objects, Scopes and Closures Project.
+
