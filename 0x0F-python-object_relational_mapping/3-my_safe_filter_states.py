4a2b0336c498ba4e6ae5cb54814aab838808e621
adonijah01 <adonijahsingoei@gmail.com>
2023-09-15 06:10:46 -0700

    ORM Fixed

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in arguments and displays all values in the states table of hbtn_0e_0_usa where name matches the argument. 
+Lists al values in the states tables of a database where name
+matches the argumenti in safe way
 """
 import sys
 import MySQLdb
@@ -12,4 +13,3 @@ if __name__ == '__main__':
     states = cur.fetchall()
     for state in states:
         print(state)
-""" adonijah kiplimo """
