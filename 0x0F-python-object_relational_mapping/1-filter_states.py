07958508d70b86fc6e83d446807491960065b0e9
adonijah01 <adonijahsingoei@gmail.com>
2023-09-15 05:48:59 -0700

    object oriented mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,24 @@
 #!/usr/bin/python3
-import MySQLdb
+"""
+Lists all states with a name starting with N
+"""
+
 import sys
-# script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa
-if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
+import MySQLdb
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3], port=3306)
 
+    cur = db.cursor()
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS \
+    LIKE 'N%';")
 
-    cursor = db.cursor()
+    states = cur.fetchall()
 
-    
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-
-    results = cursor.fetchall()
-    for row in results:
-        print(row)
-
-    
-    cursor.close()
-    db.close()
+    for state in states:
+        print(state)
 
