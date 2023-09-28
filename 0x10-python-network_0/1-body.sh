87f70d77e45e45b732834e8152637823ef37b630
adonijah01 <adonijahsingoei@gmail.com>
2023-09-28 04:22:14 -0700

    python network

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -s -o /dev/null -w "%{http_code}" "$1" | ( read status; if [ $status -eq 200 ]; then curl -s "$1"; fi )
+curl -sL "$1"
 
