f2887369cfcd8b9bdeb3deab1587d185c0a77ad2
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 17:24:50 +0300

    Error code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-""" script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8)."""
+""" script that takes in a URL, \
+        sends a request to the URL and displays\
+        the body of the response (decoded in utf-8)."""
 import urllib.request
 import sys
 import urllib.error
