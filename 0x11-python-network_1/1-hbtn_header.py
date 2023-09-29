c1d74631ca7e1cec7e795478dc0af826b50b8895
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 17:18:16 +0300

    Response header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -2,6 +2,8 @@
 """Sript that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable found in the header of the response."""
 import urllib.request
 import sys
+
+
 if __name__ == "__main__":
     url = sys.argv[1]
     r = urllib.request.Request(url)
