f9c1ad52ac9510aaf01a944035ab8cf63036c79d
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 17:40:51 +0300

    errocode updated

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""Script that takes in a URL, sends a request to the URL and displays the body of the response."""
+"""Script that takes in a URL,\
+        sends a request to the URL and displays\
+        the body of the response."""
 import requests
 import sys
 
