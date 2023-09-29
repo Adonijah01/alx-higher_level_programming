ba43d2ebd5fcf3c422041ca9a7e14f7b312c40a4
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 16:38:20 +0300

    Time for Interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""Script that takes 2 arguments in order to solve this challenge:list 10 commits (from the most recent to oldest) of a repository using use the GitHub API. and Print all commits by: `<sha>: <author name>` (one by line)"""
+import requests
+import sys
+
