1128413917f5d811b3e7f5928968096447cfdd91
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 17:49:53 +0300

    100-github commits

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-"""Script that takes your GitHub credentials (username and password) and uses the GitHub API to display your id"""
+"""Script that takes your GitHub \
+        credentials (username and password) and \
+        uses the GitHub API to display your id"""
 import sys
 import requests
 from requests.auth import HTTPBasicAuth
-
-
-
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     response = requests.get("https://api.github.com/user", auth=auth)
--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
-"""Script that takes 2 arguments in order to solve this challenge:list 10 commits (from the most recent to oldest) of a repository using use the GitHub API. and Print all commits by: `<sha>: <author name>` (one by line)"""
+"""Script that takes 2 arguments\
+        in order to solve this challenge:list 10 commits\
+        (from the most recent to oldest) of a repository\
+        using use the GitHub API. and Print \
+        all commits by: `<sha>: <author name>` (one by line)"""
 import requests
 import sys
 
@@ -7,17 +11,13 @@ if __name__ == "__main__":
 
     repo = sys.argv[1]
     owner = sys.argv[2]
-
-    
     url = 'https://api.github.com/repo/owner/commits'
-    
     response = requests.get(url)
-    
     json_resp = response.json()
 
 for commit in json_resp[:10]:
-        commit_data = commit['commit']
-        sha = commit_data.get('sha')
-        author = commit_data.get('author').get('name')
-        print(f"{sha}: {author}")
-        pass
+    commit_data = commit['commit']
+    sha = commit_data.get('sha')
+    author = commit_data.get('author').get('name')
+    print(f"{sha}: {author}")
+    pass
