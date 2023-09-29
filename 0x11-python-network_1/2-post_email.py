94a645cfabbf5335b59ef06729af981aa0cd592a
adonijah01 <adonijahsingoei@gmail.com>
2023-09-29 15:38:03 +0300

    post email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,3 +2,4 @@
 """ script that takes in a URL and an email, sends a POST request to the passed URL with the email as a parameter, and displays the body of the response (decoded in utf-8) """
 import urllib.request
 import sys
+import urllib.parse
