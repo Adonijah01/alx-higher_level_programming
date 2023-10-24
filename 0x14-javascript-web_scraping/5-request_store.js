9933d0cd4123acea1768dafd75f49b6d49797155
adonijah01 <adonijahsingoei@gmail.com>
2023-10-24 16:24:15 +0300

    Request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,30 +5,6 @@
  * of a webpage and stores it in a file
  */
 
-const request = require('request');
 const fs = require('fs');
-
-if (process.argv.length !== 4) {
-  console.error('Usage: node 5-request_store.js <URL> <file_path>');
-  process.exit(1);
-}
-
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    fs.writeFile(filePath, body, 'utf-8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log(`Data has been written to ${filePath}`);
-      }
-    });
-  } else {
-    console.error(`Error: Status code ${response.statusCode}`);
-  }
-});
-
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
