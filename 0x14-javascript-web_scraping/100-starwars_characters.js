58749a251c32c482dc25e36e15c86d191213ade1
adonijah01 <adonijahsingoei@gmail.com>
2023-10-24 16:43:13 +0300

    Right order

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,39 @@
+#!/usr/bin/node
+
+// This script prints all characters of a Star Wars movie.
+const request = require('request');
+
+if (process.argv.length !== 3) {
+  console.error('Usage: node 100-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else if (response.statusCode === 200) {
+    const movieData = JSON.parse(body);
+    const characterUrls = movieData.characters;
+    getCharacterNames(characterUrls, 0);
+  } else {
+    console.error(`Error: Status code ${response.statusCode}`);
+  }
+});
+
+function getCharacterNames(urls, index) {
+  if (index < urls.length) {
+    request(urls[index], (charError, charResponse, charBody) => {
+      if (charError) {
+        console.error(charError);
+      } else if (charResponse.statusCode === 200) {
+        const characterData = JSON.parse(charBody);
+        console.log(characterData.name);
+        getCharacterNames(urls, index + 1);
+      }
+    });
+  }
+}
+
---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+// script that computes the number of tasks completed by user id.
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+
+  if (!error) {
+
+    const todos = JSON.parse(body);
+
+    let completed = {};
+
+    todos.forEach((todo) => {
+
+      if (todo.completed && completed[todo.userId] === undefined) {
+
+        completed[todo.userId] = 1;
+
+      } else if (todo.completed) {
+
+        completed[todo.userId] += 1;
+
+      }
+
+    });
+
+    console.log(completed);
+
+  }
+
+});
