9da249a9bca6843ab7be75c74895e79542a42557
adonijah01 <adonijahsingoei@gmail.com>
2023-10-24 16:15:53 +0300

    Web scrapping fixed

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -5,29 +5,15 @@
  *  of movies where the character 
  *  “Wedge Antilles” is present.
  */
-
 const request = require('request');
-
-if (process.argv.length !== 3) {
-  console.error('Usage: node 4-starwars_count.js <API_URL>');
-  process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-const characterId = 18; // Wedge Antilles' character ID
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error(error);
-  } else if (response.statusCode === 200) {
-    const films = JSON.parse(body).results;
-    const count = films.filter((film) =>
-      film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)
-    ).length;
-
-    console.log(count);
-  } else {
-    console.error(`Error: Status code ${response.statusCode}`);
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
+      return movie.characters.find((character) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0));
   }
 });
 
